a8ce8808517a33689b5fcc5e8825f5ecad06a97d
LindaKadz <achielinda44@gmail.com>
2019-02-01 18:34:43 +0300

    add login form and display form data on console

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+   super(props);
+   this.state = {
+     firstname: '', lastname: '', username: '', email: '', password: '', confirmpassword: ''
+   };
+
+   this.handleChange = this.handleChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
+
+ validateForm() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      this.state.password.length > 8 &&
+      this.state.password === this.state.confirmpassword
     );
   }
-}
 
+
+ handleChange(event) {
+   this.setState({ [event.target.name]: event.target.value });
+ }
+
+ handleSubmit(event) {
+   event.preventDefault();
+   let data = {
+       firstname: this.state.firstname,
+       secondname: this.state.secondname,
+       username: this.state.username,
+       email: this.state.email,
+       password: this.state.password,
+       confirmpassword: this.state.confirmpassword
+      }
+console.log(data)
+ }
+
+ render() {
+   return (
+     <form onSubmit={this.handleSubmit}>
+       <label>
+         First Name:
+          <input type="text" name='firstname' onChange={this.handleChange} />
+       </label>
+       <br />
+       <br />
+
+       <label>
+         Last Name:
+         <input type="text" name='lastname' onChange={this.handleChange} />
+       </label>
+       <br />
+       <br />
+
+       <label>
+         Username:
+         <input type="text" name='username' onChange={this.handleChange} />
+       </label>
+       <br />
+       <br />
+
+       <label>
+         Email:
+         <input type="text" name='email' onChange={this.handleChange} />
+       </label>
+       <br />
+       <br />
+
+       <label>
+         Password:
+         <input type="text" name='password' onChange={this.handleChange} />
+       </label>
+       <br />
+       <br />
+
+       <label>
+         Confirm Password:
+         <input type="text" name='confirmpassword' onChange={this.handleChange} />
+       </label>
+       <br />
+       <br />
+
+       <input type="submit" value="Submit" />
+     </form>
+   );
+ }
+}
 export default App;
