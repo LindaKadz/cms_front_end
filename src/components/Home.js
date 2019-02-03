62aba6609711ed0b10e06fb7cb558e2d5478388e
LindaKadz <achielinda44@gmail.com>
2019-02-03 16:12:27 +0300

    fetch posts from rails db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,50 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    posts: []
+   }
+  }
 
-}
+  handleSubmit(event) {
+    event.preventDefault();
+  }
 
+  componentDidMount() {
+    fetch("http://localhost:3001/api/v0/contents")
+      .then(response => response.json())
+      .then(
+        (result) => {
+          let posts = result.response.map((post) => {
+            return(
+              <div key={post.id} >
+                <h2>{post.title}</h2>
+                <h4>{post.author}</h4>
+                <h4>{post.date}</h4>
+                <p>{post.summary}</p>
+                <p>{post.body}</p>
+             </div>
+            )
+          });
+          this.setState({  posts: posts});
+        },
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+
+    render() {
+      return (
+        <div>
+          Posts
+        </div>
+        { this.state.posts }
+      );
+  }
+  
 export default Home
