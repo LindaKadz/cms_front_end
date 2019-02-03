a1f6db2ffbff652efe5236c53b2afea593f68435
LindaKadz <achielinda44@gmail.com>
2019-02-03 16:04:29 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignUp from './components/Signup'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Sign Up to continue!</h1>
         </div>
-        <SignUp />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/register" component={SignUp} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
       </div>
     );
   }
